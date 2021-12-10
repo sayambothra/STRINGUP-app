4363a1c34eefe4f8f133fcc07c4674c3d25f1c1d
sayambothra <63236442+sayambothra@users.noreply.github.com>
2021-12-10 19:03:02 +0530

    Update BadmintonActivity.java

--- BadmintonActivity.java
+++ BadmintonActivity.java
@@ -2,6 +2,7 @@ package com.abdul.stringup_2o;
 
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -9,16 +10,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
+
 
 public class BadmintonActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-    String[] countryNames = {"Yonex BG-65\nRs.450/-", "Yonex BG-65 Titanium\nRs.450/-", "Yonex AeroBite\nRs.450/-", "Li-Ning No.7\nRs.450/-", "Li-Ning No.1\nRs.450/-"};
+    String[] Strings = {"Yonex BG-65\nRs.450/-", "Yonex BG-65 Titanium\nRs.450/-", "Yonex AeroBite\nRs.450/-", "Li-Ning No.7\nRs.450/-", "Li-Ning No.1\nRs.450/-"};
     int flags[] = {R.drawable.bg_65, R.drawable.bg_65titanium, R.drawable.aerobyte, R.drawable.lining1, R.drawable.lining7};
-
+    EditText brand,model;
+    Button click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +30,7 @@ public class BadmintonActivity extends AppCompatActivity implements AdapterView.
         Spinner spin = (Spinner) findViewById(R.id.bString);
         spin.setOnItemSelectedListener(this);
 
-        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), flags, countryNames);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), flags, Strings);
         spin.setAdapter(customAdapter);
         Spinner spinner1 = (Spinner) findViewById(R.id.bCover);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -45,9 +48,33 @@ public class BadmintonActivity extends AppCompatActivity implements AdapterView.
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner2.setAdapter(adapter2);
+
+
+        brand=findViewById(R.id.bBrand);
+        model=findViewById(R.id.bModel);
+        Spinner spinner = (Spinner)findViewById(R.id.bString);
+
+
+        click=findViewById(R.id.bnext);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BadmintonActivity.this,BRdetailsActivity.class);
+                String bbrand=brand.getText().toString();
+                String bmodel=model.getText().toString();
+
+                String text = spinner.getSelectedItem().toString();
+
+                intent.putExtra("Value",bbrand);
+                intent.putExtra("Name",bmodel);
+                intent.putExtra("Type",text);
+                startActivity(intent);
+
+            }
+        });
+        
     }
-
-
+    
     //Performing action onItemSelected and onNothing selected
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
@@ -71,44 +98,13 @@ public class BadmintonActivity extends AppCompatActivity implements AdapterView.
 
 
         return super.onOptionsItemSelected(item);
-    }
 
-   public void brDetails(View view) {
-////        Intent intent=new Intent(this,BRdetailsActivity.class);
-////        startActivity(intent);
-    EditText bbrand = findViewById(R.id.bBrand);
-    EditText bmodel = findViewById(R.id.bModel);
-    Spinner btype = findViewById(R.id.bString);
-    Spinner btension = findViewById(R.id.bTension);
-    Spinner bcover = findViewById(R.id.bCover);
-    String name = bbrand.getText().toString();
-    String model = bmodel.getText().toString();
-
-    String type = btype.getSelectedItem().toString();
-    String tension = btension.getSelectedItem().toString();
-    String cover = bcover.getSelectedItem().toString();
-
-            Toast.makeText(getApplicationContext(), name + " " + model + " " + type + " " + tension + " " + cover, Toast.LENGTH_LONG).show();
+//  
             }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
 
 
 
